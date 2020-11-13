d1b71bd37832963aee21c45a2fb109254ed7187d
rihabek <rihabelkhadiri@hotmail.com>
2020-11-13 20:31:30 +0100

    SDL

--- main.c
+++ main.c
@@ -6,7 +6,30 @@
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  // initialisation SDL
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
+      fprintf(stderr, "Could not initialize sdl2: %s\n", SDL_GetError());
 
-  return 0;
+      return EXIT_FAILURE;
+  }
+
+  SDL_Window *window = SDL_CreateWindow("Procedural",100, 100,SCREEN_WIDTH, SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
+
+  if (window == NULL)
+  {
+      fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+
+      return EXIT_FAILURE;
+  }
+  
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+                                              SDL_RENDERER_ACCELERATED |
+                                              SDL_RENDERER_PRESENTVSYNC);
+  if (renderer == NULL) {
+      SDL_DestroyWindow(window);
+      fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+      return EXIT_FAILURE;
+  }
+
 }
